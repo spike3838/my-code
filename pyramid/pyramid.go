06a37eb8da3249a43a8bd890f733e3c634051c52
Marvin <spike3838@googlemail.com>
2016-09-02 14:46:11 +0200

    first running version

--- pyramid/pyramid.go
+++ pyramid/pyramid.go
@@ -5,28 +5,32 @@ import "fmt"
 
 func pyramid(pyramid string) string {
 
-	length := len(pyramid)
-
-	if length < 3 {
+	if len(pyramid) < 3 {
 	return "Error: Need at least 3 args"
 	}
 
-
+	white_space := " "
 	state := pyramid
-	for {
+	for i := 0; i < len(pyramid); i++{
 
+		buffer := ""
 		new_level := switchandremove(state)
 		state = new_level
 
-		if len(new_level) < 2 {
-		return pyramid
-		} else {
+		for j := 0; j <= i; j++{
 
-		new_level += "\n"
-		new_level += pyramid
-		pyramid = new_level
+			buffer += white_space
 		}
 
+		if len(new_level) < 2 {
+			return pyramid
+		} else {
+			new_level += "\n"
+			buffer += new_level
+			new_level = buffer
+			new_level += pyramid
+			pyramid = new_level
+		}
 	}
 
 	return pyramid
@@ -36,7 +40,7 @@ func switchandremove(level string) string{
 
 	level_byte := []byte(level)
 	level_slice := []byte(level[1:len(level_byte)-1])
-
+//	level_slice := []byte(remove_edge(level))
 
 	for i := 0 ; i < len(level_slice); i++{
 
@@ -51,15 +55,12 @@ func switchandremove(level string) string{
 			}
 		}
 	}
-
 	return string(level_slice)
 }
-
-
-
 
 
 func main() {
 	base := "/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\"
 	fmt.Println(pyramid(base))
+
 }
